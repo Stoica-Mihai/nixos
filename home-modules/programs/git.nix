9e3680bba890163dc1ee0debfbad1c05d07fb587
MCS <email@gmail.com>
2025-09-23 22:03:09 +0300

    - Added aliases to git config - Added more config to vim

--- home-modules/programs/git.nix
+++ home-modules/programs/git.nix
@@ -9,5 +9,12 @@
     extraConfig = {
      core.editor = "vim";
     };
+
+    aliases = {
+      c = "commit";
+      s = "status";
+      l = "log --color --graph --pretty=format:'%C(cyan)%h%Creset -%C(yellow)%d%Creset %s %C(green)(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      ac = "!git add -A && git commit";
+    };
   };
 }
--- home-modules/programs/vim.nix
+++ home-modules/programs/vim.nix
@@ -4,6 +4,12 @@
   programs.vim = {
     enable = true;
     defaultEditor = true;
+    settings = {
+      tabstop = 2;
+      shiftwidth = 2;
+      expandtab = true;
+    };
+
     plugins = with pkgs.vimPlugins; [
       ale
       airline
