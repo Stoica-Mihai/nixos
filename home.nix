ddfae548e9a321c633341f688693c943387dfe4e
MCS <email@gmail.com>
2025-09-23 20:01:12 +0300

    - Removed hardware-configuration.nix since it should not be reused in my   opinion and only use what you get when you install it - Added configuration for lxqt environment and updated home.nix with it

--- home.nix
+++ home.nix
@@ -7,6 +7,7 @@
   home.file.".wezterm.lua".source = ./dotfiles/wezterm/.wezterm.lua; 
   home.file.".config/fish/config.fish".source = ./dotfiles/fish/config.fish; 
   home.file.".config/fish/functions/fish_prompt.fish".source = ./dotfiles/fish/fish_prompt.fish;
+  home.file.".config/lxqt".source = ./dotfiles/lxqt;
 
   imports = [
     ./home-modules/programs
