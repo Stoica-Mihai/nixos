36a864dc34ea5bd768de7062e6c8b26282c45367
MeNotSanta <37269976+Stoica-Mihai@users.noreply.github.com>
2025-10-28 11:37:18 +0200

    Update WezTerm configuration with new settings

--- dotfiles/wezterm/.wezterm.lua
+++ dotfiles/wezterm/.wezterm.lua
@@ -4,22 +4,41 @@ local wezterm = require 'wezterm'
 -- Holds configuration
 local config = wezterm.config_builder()
 
+-- Environment variables
+config.set_environment_variables = {
+  EDITOR = 'vim',
+  STARSHIP_CONFIG = wezterm.home_dir .. '/.config/starship/starship.toml',
+  ADFS_USER = '' -- email,
+  fish_greeting = "", -- disable fish shell greeting
+}
+
 -- Window size
 config.initial_cols = 120
 config.initial_rows = 30
 
 -- Looks
-config.color_scheme = "MaterialDesignColors"
+config.color_scheme = "Dracula"
 config.window_background_opacity = 0.9
 config.default_cursor_style = 'SteadyUnderline'
 config.use_fancy_tab_bar = false
 config.enable_tab_bar = false
+config.window_padding = {
+  left = 8,
+  right = 8,
+  top = 0,
+  bottom = 8
+}
 
 -- Behavior
 config.window_close_confirmation = 'AlwaysPrompt'
+config.warn_about_missing_glyphs = false
 
 -- Font
-config.font_size = 14
+config.font = wezterm.font_with_fallback {
+    {family="JetBrains Mono NL", weight="Medium"},
+    'JetBrains Mono',
+}
+config.font_size = 12
 
 -- Shell definition
 config.default_prog = { "fish", "-l" }
@@ -37,8 +56,8 @@ config.keys = {
     -- Fullscreen
     { key = "f", mods="SUPER", action="ToggleFullScreen" },
     -- Scrolling
-    { key = "UpArrow", mods = "SUPER", action = wezterm.action.ScrollByLine(-1) },
-    { key = "DownArrow", mods = "SUPER", action = wezterm.action.ScrollByLine(1) }
+    { key = "UpArrow", mods = "CTRL", action = wezterm.action.ScrollByLine(-1) },
+    { key = "DownArrow", mods = "CTRL", action = wezterm.action.ScrollByLine(1) }
 }
 
 -- Export config
